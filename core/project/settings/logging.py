1552de766fe05f08447841c9ee0d0fe493bb1c32
AdityaTeng <adityateng@gmail.com>
2024-03-21 10:24:51 -0400

    Vid2 complete, Vid3 Logging continue

---
+++ core/project/settings/logging.py
@@ -0,0 +1,10 @@
+LOGGING = {
+    'version' : 1,
+    'disable_existing_loggers' : False,
+    'formatters' : {
+        'standard' : {
+            'format' : '%(asctime)s %(levelname)s %(name)s %(message)s'
+        }
+    }
+
+}
